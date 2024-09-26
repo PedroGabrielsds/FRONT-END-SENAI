48fc9d877cdb1d3087499bb24e15d64ffba43195
PedroGabrielsds <pedrogabrielsilva.d.s@gmail.com>
2024-09-26 09:26:58 -0300

    Correção atividades aula 5

--- Scripts/Atividades js/Atividade aula 5/scripts/scripts.js
+++ Scripts/Atividades js/Atividade aula 5/scripts/scripts.js
@@ -1,5 +1,5 @@
 function Filmes(){
-    var Filmes = ["Missão Impossivel", "John Wick", "Jack Witcher", "Top-Gun", "Iron-Man", "Capitão America" ]
+    var Filmes = ["Missão Impossivel", " John Wick", " Jack Witcher", " Top-Gun", " Iron-Man", " Capitão America" ]
     document.getElementById("Catalogo").innerHTML += `
         <div class="Elementos">
 
@@ -9,18 +9,50 @@ function Filmes(){
     `
 }
 
-function tabuada(){
-    var Num = document.getElementById("Conta").value
-    var Contador = 0
 
-    while(Contador <= 10){
-        document.getElementById("Resposta").innerHTML += `
-            <div class="Elementos">
-                ${Num * Contador}
-                ${Contador++}
-            </div>
-            `
-    }
+/*  
+    Correção Atividades aula 5
     
-    
+// 1 - Faça uma listagem de 6 filmes de sua escolha com array e faça eles serem impressos no html
+
+function ex1(){
+    var filmes = ['Senhor do aneis','Star wars','Matrix','Dune','Dune2','Vingadores']
+
+    filmes.forEach(cadaFilme => {
+        document.querySelector("#respostaEx1").innerHTML += `
+            <p> ${cadaFilme} </p>
+        `
+    });
 }
+
+// 2 - Fazer a tabuada com repetições recebendo os valores pelos inputs
+    PASSO 1
+    - BUSCAR INPUT E RECEBER O VALOR
+
+   PASSO 2
+    - PEGAR O VALOR E MULTIPLICAR ATE 10
+
+   PASSO 3
+    - IMPRIMIR O RESULTADO NA DIV RESPOSTAEX2
+
+function ex2(){
+    let numero = Number(document.querySelector("#inputEx2").value)
+
+    // let contador = 0
+    // while(contador <= 10){
+
+    //     contador++
+    // }
+    if(numero == 0){
+        document.querySelector("#respostaEx2").innerHTML = ``
+    }else{
+        for(let contador = 0;contador <= 10;contador++){
+        
+            let calculo = numero * contador
+    
+            document.getElementById("respostaEx2").innerHTML += `
+                <p> ${numero} x ${contador} = ${calculo} </p>
+            `
+        }
+    }*/
+
